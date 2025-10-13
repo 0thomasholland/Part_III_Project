f856acff47fadf0237e97bc0afd994c5dbae9102
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-10-13 19:09:56 +0100

    Updated the code a bit

--- src/Part_III_Project/sea_surface_height.py
+++ src/Part_III_Project/sea_surface_height.py
@@ -1,8 +1,3 @@
-def dSL_to_dSSH(
-    delta_sea_level,
-    displacement,
-    delta_gravity_potential):
-        delta_sea_surface = (
-            delta_sea_level + displacement + (delta_gravity_potential / 9.81)
-        )
-        return delta_sea_surface
+def dSL_to_dSSH(delta_sea_level, delta_gravity_potential):
+    delta_sea_surface = delta_sea_level + (delta_gravity_potential / 9.81)
+    return delta_sea_surface
