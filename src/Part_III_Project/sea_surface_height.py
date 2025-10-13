782f7993c30f60d83abe2cc12300631774cafeb5
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-10-13 17:21:23 +0100

    add in sea level to sea surface code

---
+++ src/Part_III_Project/sea_surface_height.py
@@ -0,0 +1,10 @@
+def dSL_to_dSSH(
+    delta_sea_level,
+    displacement,
+    delta_gravity_potential):
+        delta_sea_surface = (
+            delta_sea_level
+            + displacement
+            + delta_gravity_potential / 9.81
+        )
+        return delta_sea_surface
