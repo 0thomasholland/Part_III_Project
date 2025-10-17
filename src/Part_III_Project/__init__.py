11b38e1abdbc7237303d3fa4d72a1734e0500720
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-10-17 17:34:21 +0100

    added error estimations

--- src/Part_III_Project/__init__.py
+++ src/Part_III_Project/__init__.py
@@ -3,7 +3,9 @@ Unified public imports for the library
 """
 
 from Part_III_Project.sea_surface_height import SeaSurfaceFingerPrint
+from Part_III_Project.sea_surface_height_change import sea_surface_height_change
 
 __all__ = [
     "SeaSurfaceFingerPrint",
+    "sea_surface_height_change",
 ]
