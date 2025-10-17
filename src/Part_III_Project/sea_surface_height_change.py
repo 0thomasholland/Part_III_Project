48659f8185fe6955074dd5116daaac2fe6005d49
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-10-17 17:34:21 +0100

    Add error estimation for traditional altimetry method with ternary analysis

---
+++ src/Part_III_Project/sea_surface_height_change.py
@@ -0,0 +1,18 @@
+from pyshtools import SHGrid
+from pyslfp.physical_parameters import GRAVITATIONAL_ACCELERATION
+
+
+def sea_surface_height_change(finger_print, sea_level_change, displacement, angular_velocity_change
+) -> SHGrid:
+    """Calculate the sea surface height change from the sea level change,
+    surface displacement, and angular velocity change.
+
+    Args:
+        sea_level_change (SHGrid): The change in sea level.
+        displacement (SHGrid): The vertical surface displacement.
+        angular_velocity_change (SHGrid): The change in angular velocity.
+
+    Returns:
+        SHGrid: The change in sea surface height.
+    """
+    return sea_level_change + displacement + (finger_print.centrifugal_potential_change(angular_velocity_change=angular_velocity_change)/GRAVITATIONAL_ACCELERATION)
