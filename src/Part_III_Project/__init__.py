ec154784ccf5175313677e85dcfa46b112eaaac5
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-10-20 11:09:24 +0100

    re-run at higher resolution

--- src/Part_III_Project/__init__.py
+++ src/Part_III_Project/__init__.py
@@ -5,7 +5,7 @@ Unified public imports for the library
 from Part_III_Project.plot_methods import (
     plot_ternary_heatmap,
     plot_ternary_heatmap_subplots,
-    plot_ternary_heatmap_subplots_parallel,
+    # plot_ternary_heatmap_subplots_parallel,
 )
 from Part_III_Project.sea_surface_height import SeaSurfaceFingerPrint
 from Part_III_Project.sea_surface_height_change import sea_surface_height_change
@@ -17,6 +17,6 @@ __all__ = [
     "sea_surface_height_change",
     "plot_ternary_heatmap",
     "plot_ternary_heatmap_subplots",
-    "plot_ternary_heatmap_subplots_parallel",
+    # "plot_ternary_heatmap_subplots_parallel",
     "create_ice_band",
 ]
