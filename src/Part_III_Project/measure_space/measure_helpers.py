629fb38900d793972312b4ec456cf961f82b48c6
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-04 17:06:29 +0000

    laying out measure space code

---
+++ src/Part_III_Project/measure_space/measure_helpers.py
@@ -0,0 +1,11 @@
+from pygeoinf.gaussian_measure import GaussianMeasure
+
+
+def return_expectation(measure: GaussianMeasure) -> float:
+    # return measure.expectation
+    pass
+
+
+def return_1D_variance(measure: GaussianMeasure) -> float:
+    # return measure.covariance[0, 0]
+    pass
