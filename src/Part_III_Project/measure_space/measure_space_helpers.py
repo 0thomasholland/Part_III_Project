a80d6ea60ae3f82437f713573049b697e4572848
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-05 20:30:18 +0000

    finished off the measure functions, now to implement...

--- src/Part_III_Project/measure_space/measure_space_helpers.py
+++ src/Part_III_Project/measure_space/measure_space_helpers.py
@@ -2,34 +2,9 @@ from pygeoinf.gaussian_measure import GaussianMeasure
 from pygeoinf.linear_operators import LinearOperator
 
 
-def return_expectation(measure: GaussianMeasure) -> float:
-    # return measure.expectation
-    pass
-
-
-def return_1D_variance(measure: GaussianMeasure) -> float:
-    # return measure.covariance[0, 0]
-    pass
-
-
-def check_space_compatibility(
-    measure1: GaussianMeasure, measure2: GaussianMeasure
-) -> bool:
-    """
-    Check if two Gaussian measures are compatible in terms of their
-    spatial domains and covariances.
-    """
-    if measure1.domain != measure2.domain:
-        return False
-    return True
-
-
-def check_operator_compatibility(
-    operator: LinearOperator, input_measure: GaussianMeasure
-) -> bool:
-    """
-    Check if a linear operator is compatible with given input and output spaces.
-    """
-    if operator.domain != input_measure.domain:
-        return False
-    return True
+def get_stats_from_measure(
+    measure: GaussianMeasure,
+) -> tuple[float, float]:
+    expectation = measure.expectation[0]
+    variance = measure.covariance.matrix(dense=True)[0, 0]
+    return expectation, variance
