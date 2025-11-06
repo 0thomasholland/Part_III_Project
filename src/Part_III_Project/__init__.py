5cfb96d17ddf1a16a795f26d617702e6daa5f399
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-06 17:49:16 +0000

    updated measures

--- src/Part_III_Project/__init__.py
+++ src/Part_III_Project/__init__.py
@@ -1,8 +1,8 @@
-"""
-Unified public imports for the library
-"""
+"""Unified public imports for the library"""
 
-from Part_III_Project.deterministic_space.load_generators import create_ice_band
+from Part_III_Project.deterministic_space.load_generators import (
+    create_ice_band,
+)
 from Part_III_Project.deterministic_space.plot_methods import (
     plot_ternary_heatmap,
     plot_ternary_heatmap_subplots,
@@ -13,14 +13,17 @@ from Part_III_Project.deterministic_space.sea_surface_height_change import (
     sea_surface_height_change,
 )
 from Part_III_Project.measure_space.measure_space_helpers import (
-    return_1D_variance,
-    return_expectation,
+    get_stats_from_measure,
 )
 from Part_III_Project.measure_space.measures import (
-    altimetry_observed_sea_surface_change_measure,
-    calculate_measures,
+    altimetry_measurements_measure,
+    gmsl_measure,
+    ice_thickness_change_measures,
+    load_measure,
+    ocean_dynamic_topography_measures,
     sea_level_change_measure,
-    sea_surface_change_measure,
+    sea_surface_height_measure,
+    sensor_error_measure,
 )
 
 __all__ = [
@@ -31,9 +34,12 @@ __all__ = [
     # "plot_ternary_heatmap_subplots_parallel",
     "create_ice_band",
     "sea_level_change_measure",
-    "sea_surface_change_measure",
-    "altimetry_observed_sea_surface_change_measure",
-    "calculate_measures",
-    "return_expectation",
-    "return_1D_variance",
+    "ice_thickness_change_measures",
+    "ocean_dynamic_topography_measures",
+    "load_measure",
+    "gmsl_measure",
+    "altimetry_measurements_measure",
+    "sensor_error_measure",
+    "get_stats_from_measure",
+    "sea_surface_height_measure",
 ]
--- src/Part_III_Project/measure_space/operators.py
+++ src/Part_III_Project/measure_space/operators.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from pygeoinf import (
     GaussianMeasure,
     LinearOperator,
@@ -5,6 +7,7 @@ from pygeoinf import (
 )
 from pyslfp import (
     FingerPrint,
+    averaging_operator,
     ice_thickness_change_to_load_operator,
     sea_level_change_to_load_operator,
 )
@@ -12,5 +15,54 @@ from pyslfp import (
 
 def gmsl_measure_condenser(
     measure: GaussianMeasure,
-) -> GaussianMeasure:
-    pass
+    fingerprint: FingerPrint,
+) -> LinearOperator:
+    weighting_function = (
+        -fingerprint.ice_density
+        * fingerprint.one_minus_ocean_function
+        * fingerprint.ice_projection(value=0)
+        * fingerprint.length_scale
+        / (fingerprint.water_density * fingerprint.ocean_area)
+    )
+    gmsl_operator = averaging_operator(
+        measure.domain,
+        [weighting_function],
+    )
+    return gmsl_operator
+
+
+"""
+def remove_ice_average_operator(
+    finger_print: FingerPrint,
+    fingerprint_operator: LinearOperator,
+):
+    l2_load_space = underlying_space(fingerprint_operator.domain)
+
+    ice_function = finger_print.ice_function
+    ice_area = finger_print.ice_area
+
+    def mapping(load):
+        ocean_average = (
+            finger_print.integrate(ocean_function * load) / ocean_area
+        )
+        new_load = load.copy()
+        new_load.data -= ocean_average
+        return new_load
+
+    def adjoint_mapping(load):
+        average = finger_print.integrate(load)
+        return load - average * ocean_function / ocean_area
+
+    l2_operator = LinearOperator(
+        l2_load_space,
+        l2_load_space,
+        mapping,
+        adjoint_mapping=adjoint_mapping,
+    )
+
+    return LinearOperator.from_formal_adjoint(
+        load_space,
+        load_space,
+        l2_operator,
+    )
+"""
