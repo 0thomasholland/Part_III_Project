9d1de41e95f80c46487ca89acc4361a4016567db
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-07 21:21:53 +0000

    Optimise variable inputs distribution mapping script

--- work/5-distribution_mapping/variable_inputs.py
+++ work/5-distribution_mapping/variable_inputs.py
@@ -1,3 +1,4 @@
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -14,8 +15,12 @@ from Part_III_Project import (
     sea_surface_height_measure,
 )
 
+verbosity = 1
+# %%
+print("Starting variable input GMSL estimation script")
+
 # --- Set up a fingerprint instance ---
-fp = sl.FingerPrint(lmax=128)
+fp = sl.FingerPrint(lmax=64)
 fp.set_state_from_ice_ng()
 
 fingerprint_operator = fp.as_sobolev_linear_operator(
@@ -26,145 +31,199 @@ fingerprint_operator = fp.as_sobolev_linear_operator(
 
 odt_length_scales = (
     # np.linspace(0.01, 0.5, 10) * fp.mean_sea_floor_radius
-    np.linspace(0.01, 0.5, 3) * fp.mean_sea_floor_radius
+    np.linspace(0.01, 0.5, 2) * fp.mean_sea_floor_radius
 )
 odt_amplitude_95_ranges = np.array(
     # [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 10],
-    [0.001, 0.01, 0.1, 1.0, 10],
+    [0.01, 0.1, 10],
 )  # in units of sea level
 
 ice_length_scales = (
     # np.linspace(0.1, 0.5, 10) * fp.mean_sea_floor_radius
-    np.linspace(0.1, 0.5, 3) * fp.mean_sea_floor_radius
+    np.linspace(0.1, 0.5, 2) * fp.mean_sea_floor_radius
 )
 ice_thickness_95_ranges = np.array(
     # [1, 10, 25, 50, 100, 200, 300, 400, 500],
-    [25, 100, 250, 500],
+    [100, 500],
 )  # in meters
 # net_ice_thickness_changes = np.linspace(-200, 200, 20)  # in meters
-net_ice_thickness_changes = np.linspace(-200, 200, 4)  # in meters
+net_ice_thickness_changes = np.linspace(-200, 200, 2)  # in meters
 
 
-load_data = pd.DataFrame(
-    columns=[
-        "odt_length_scale",
-        "odt_amplitude_95_range",
-        "ice_length_scale",
-        "ice_thickness_95_range",
-        "net_ice_thickness_change",
-        "ice_load_measure",
-        "ocean_dynamic_load_measure",
-        "ocean_dynamic_measure",
-    ],
-)
+odt_length_scale = []
+odt_amplitude_95_range = []
+ice_length_scale = []
+ice_thickness_95_range = []
+net_ice_thickness_change = []
+ice_load_measure = []
+ocean_dynamic_load_measure = []
+ocean_dynamic_measure = []
+# %%
+print("Generating measures for variable inputs...")
 
 
 # make joblib parallel for each combination of inputs
-def generate_measures(
-    odt_length_scale: float,
-    odt_amplitude_95_range: float,
-    ice_length_scale: float,
-    ice_thickness_95_range: float,
-    net_ice_thickness_change: float,
+def return_odt_measure(
+    fingerprint,
+    fingerprint_operator,
+    length_scale,
+    amplitude_95_range,
+):
+    odt_measure, odt_load_measure = ocean_dynamic_topography_measures(
+        fingerprint=fingerprint,
+        fingerprint_operator=fingerprint_operator,
+        length_scale=length_scale,
+        amplitude_95_range=amplitude_95_range,
+    )
+    return (
+        odt_measure,
+        odt_load_measure,
+        length_scale,
+        amplitude_95_range,
+    )
+
+
+def return_ice_measure(
+    fingerprint,
+    fingerprint_operator,
+    length_scale,
+    thickness_95_range,
+    net_thickness_change,
 ):
     _, ice_load_measure = ice_thickness_change_measures(
+        fingerprint=fingerprint,
+        fingerprint_operator=fingerprint_operator,
+        length_scale=length_scale,
+        thickness_95_range=thickness_95_range,
+        net_thickness_change=net_thickness_change,
+    )
+    return (
+        length_scale,
+        thickness_95_range,
+        net_thickness_change,
+        ice_load_measure,
+    )
+
+
+print(
+    "number of combinations:",
+    len(odt_length_scales) * len(odt_amplitude_95_ranges)
+    + len(ice_length_scales)
+    * len(ice_thickness_95_ranges)
+    * len(net_ice_thickness_changes),
+)
+# %%
+ice_results = Parallel(n_jobs=-1, verbose=verbosity)(
+    delayed(return_ice_measure)(
         fingerprint=fp,
         fingerprint_operator=fingerprint_operator,
         length_scale=ice_length_scale,
         thickness_95_range=ice_thickness_95_range,
         net_thickness_change=net_ice_thickness_change,
     )
-    (
-        ocean_dynamic_measure,
-        ocean_dynamic_load_measure,
-    ) = ocean_dynamic_topography_measures(
+    for ice_length_scale in ice_length_scales
+    for ice_thickness_95_range in ice_thickness_95_ranges
+    for net_ice_thickness_change in net_ice_thickness_changes
+)
+odt_results = Parallel(n_jobs=-1, verbose=verbosity)(
+    delayed(return_odt_measure)(
         fingerprint=fp,
         fingerprint_operator=fingerprint_operator,
         length_scale=odt_length_scale,
         amplitude_95_range=odt_amplitude_95_range,
     )
-    load_data_row = {
-        "odt_length_scale": odt_length_scale,
-        "odt_amplitude_95_range": odt_amplitude_95_range,
-        "ice_length_scale": ice_length_scale,
-        "ice_thickness_95_range": ice_thickness_95_range,
-        "net_ice_thickness_change": net_ice_thickness_change,
-        "ice_load_measure": ice_load_measure,
-        "ocean_dynamic_load_measure": ocean_dynamic_load_measure,
-        "ocean_dynamic_measure": ocean_dynamic_measure,
-    }
-    return load_data_row
-
-
-print(
-    "number of combinations:",
-    len(odt_length_scales)
-    * len(odt_amplitude_95_ranges)
-    * len(ice_length_scales)
-    * len(ice_thickness_95_ranges)
-    * len(net_ice_thickness_changes),
-)
-
-results = Parallel(n_jobs=-1, verbose=4)(
-    delayed(generate_measures)(
-        odt_length_scale,
-        odt_amplitude_95_range,
-        ice_length_scale,
-        ice_thickness_95_range,
-        net_ice_thickness_change,
-    )
     for odt_length_scale in odt_length_scales
     for odt_amplitude_95_range in odt_amplitude_95_ranges
-    for ice_length_scale in ice_length_scales
-    for ice_thickness_95_range in ice_thickness_95_ranges
-    for net_ice_thickness_change in net_ice_thickness_changes
 )
+for res in ice_results:
+    (
+        ice_length_scale_out,
+        ice_thickness_95_range_out,
+        ice_net_thickness_change_out,
+        ice_load_measure_out,
+    ) = res
+    for odt_res in odt_results:
+        (
+            odt_measure_out,
+            odt_load_measure_out,
+            odt_length_scale_out,
+            odt_amplitude_95_range_out,
+        ) = odt_res
+        odt_length_scale.append(odt_length_scale_out)
+        odt_amplitude_95_range.append(odt_amplitude_95_range_out)
+        ice_length_scale.append(ice_length_scale_out)
+        ice_thickness_95_range.append(ice_thickness_95_range_out)
+        net_ice_thickness_change.append(ice_net_thickness_change_out)
+        ice_load_measure.append(ice_load_measure_out)
+        ocean_dynamic_load_measure.append(odt_load_measure_out)
+        ocean_dynamic_measure.append(odt_measure_out)
 
-data = pd.DataFrame(results)
+# %%
+## generate the direct loads for each combination of inputs and add to list
+
+direct_loads = []
+
+print("Computing direct loads for each combination of inputs...")
+print("Total combinations:", len(ice_load_measure))
 
 
-## generate the direct loads for each row and add to the dataframe using joblib parallel
-def compute_direct_loads(row):
+def compute_direct_loads(
+    ice_load_measure,
+    ocean_dynamic_load_measure,
+):
     direct_load = load_measure(
-        ice_thickness_load_measure=row["ice_load_measure"],
-        odt_load_measure=row["ocean_dynamic_load_measure"],
+        ice_thickness_load_measure=ice_load_measure,
+        odt_load_measure=ocean_dynamic_load_measure,
     )
     return direct_load
 
 
-data["direct_load_measure"] = Parallel(n_jobs=-1, verbose=4)(
-    delayed(compute_direct_loads)(row) for _, row in data.iterrows()
-)
-
-
-# multiple job libs for gmsl true and ssh estimates that all run at once
-def gmsl_via_slc(row):
-    gmsl_true = gmsl_measure(
-        measure=sea_level_change_measure(
-            fingerprint_operator=fingerprint_operator,
-            fingerprint=fp,
-            load_measure=row["direct_load_measure"],
-        ),
-        fingerprint=fp,
+direct_loads = Parallel(n_jobs=-1, verbose=verbosity)(
+    delayed(compute_direct_loads)(
+        ice_load_measure[i],
+        ocean_dynamic_load_measure[i],
     )
-    expectation, variance = get_stats_from_measure(gmsl_true)
-    return expectation, np.sqrt(variance)
+    for i in range(len(ice_load_measure))
+)
+# %%
+print("Computed all direct loads, now computing responses metrics...")
 
 
-def gmsl_via_ssh(row):
+def gmsl_via_slc(fingerprint, fingerprint_operator, direct_load):
+    slc = sea_level_change_measure(
+        fingerprint_operator=fingerprint_operator,
+        fingerprint=fingerprint,
+        direct_load_measure=direct_load,
+    )
+    gmsl_slc_estimate = gmsl_measure(
+        measure=slc,
+        fingerprint=fingerprint,
+    )
+    slc_expectation, slc_variance = get_stats_from_measure(
+        gmsl_slc_estimate,
+    )
+    return slc_expectation, np.sqrt(slc_variance)
+
+
+def gmsl_via_ssh(
+    fingerprint,
+    fingerprint_operator,
+    direct_load,
+    ocean_dynamic_measure,
+):
     ssh, ssh_odt, _ = sea_surface_height_measure(
         fingerprint_operator=fingerprint_operator,
-        fingerprint=fp,
-        load_measure=row["direct_load_measure"],
-        ocean_dynamic_measure=row["ocean_dynamic_measure"],
+        fingerprint=fingerprint,
+        load_measure=direct_load,
+        ocean_dynamic_measure=ocean_dynamic_measure,
     )
     gmsl_ssh_estimate = gmsl_measure(
         measure=ssh,
-        fingerprint=fp,
+        fingerprint=fingerprint,
     )
     gmsl_ssh_odt_estimate = gmsl_measure(
         measure=ssh_odt,
-        fingerprint=fp,
+        fingerprint=fingerprint,
     )
     ssh_expectation, ssh_variance = get_stats_from_measure(
         gmsl_ssh_estimate,
@@ -180,26 +239,61 @@ def gmsl_via_ssh(row):
     )
 
 
-# select the input variables into new dataframe
-gmsl_data = data[
-    [
-        "odt_length_scale",
-        "odt_amplitude_95_range",
-        "ice_length_scale",
-        "ice_thickness_95_range",
-        "net_ice_thickness_change",
-    ]
-].copy()
+gmsl_slc_expectation = []
+gmsl_slc_std = []
+gmsl_ssh_expectation = []
+gmsl_ssh_std = []
+gmsl_ssh_odt_expectation = []
+gmsl_ssh_odt_std = []
 
-# calc stats
-gmsl_data[["true_mean", "true_std"]] = Parallel(n_jobs=-1, verbose=4)(
-    delayed(gmsl_via_slc)(row) for _, row in data.iterrows()
+# use joblib to parallelize the computation of gmsl estimates adding ssh and slc to same joblist
+print("Number of jobs: ", len(direct_loads))
+
+results_gmsl = Parallel(n_jobs=-1, verbose=verbosity)(
+    delayed(
+        lambda i: (
+            *gmsl_via_slc(
+                fingerprint=fp,
+                fingerprint_operator=fingerprint_operator,
+                direct_load=direct_loads[i],
+            ),
+            *gmsl_via_ssh(
+                fingerprint=fp,
+                fingerprint_operator=fingerprint_operator,
+                direct_load=direct_loads[i],
+                ocean_dynamic_measure=ocean_dynamic_measure[i],
+            ),
+        ),
+    )(i)
+    for i in range(len(direct_loads))
 )
-gmsl_data[["ssh_mean", "ssh_std", "ssh_odt_mean", "ssh_odt_std"]] = (
-    Parallel(n_jobs=-1, verbose=4)(
-        delayed(gmsl_via_ssh)(row) for _, row in data.iterrows()
-    )
+for res in results_gmsl:
+    gmsl_slc_expectation.append(res[0])
+    gmsl_slc_std.append(res[1])
+    gmsl_ssh_expectation.append(res[2])
+    gmsl_ssh_std.append(res[3])
+    gmsl_ssh_odt_expectation.append(res[4])
+    gmsl_ssh_odt_std.append(res[5])
+# %%
+print("Computed all GMSL estimates, now saving results...")
+
+# --- Save results to a dataframe ---
+df = pd.DataFrame(
+    {
+        "odt_length_scale": odt_length_scale,
+        "odt_amplitude_95_range": odt_amplitude_95_range,
+        "ice_length_scale": ice_length_scale,
+        "ice_thickness_95_range": ice_thickness_95_range,
+        "net_ice_thickness_change": net_ice_thickness_change,
+        "gmsl_slc_expectation": gmsl_slc_expectation,
+        "gmsl_slc_std": gmsl_slc_std,
+        "gmsl_ssh_expectation": gmsl_ssh_expectation,
+        "gmsl_ssh_std": gmsl_ssh_std,
+        "gmsl_ssh_odt_expectation": gmsl_ssh_odt_expectation,
+        "gmsl_ssh_odt_std": gmsl_ssh_odt_std,
+    },
 )
 
-# dump data to file as csv
-gmsl_data.to_csv("gmsl_data.csv", index=False)
+df.to_csv("variable_input_gmsl_estimates_low_rest.csv", index=False)
+
+print("Saved results to csv")
