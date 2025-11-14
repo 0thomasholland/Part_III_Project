bf9cf8db7f84416148c694985ae21744f168a7d8
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-14 11:48:58 +0000

    refactor single value example

--- work/5-distribution_mapping/single_value_example.py
+++ work/5-distribution_mapping/single_value_example.py
@@ -56,6 +56,56 @@ ocean_dynamic_measure, ocean_dynamic_load_measure = (
     )
 )
 
+# %%
+ice_thickness_measure, ice_load_measure = (
+    ice_thickness_change_measures(
+        fingerprint=fp,
+        fingerprint_operator=fingerprint_operator,
+        length_scale=ice_length_scale,
+        ice_gmsl_target_std=ice_gmsl_target_std,
+        net_thickness_change=net_ice_thickness_change,
+    )
+)
+
+# %%
+direct_load_measure = load_measure(
+    ice_thickness_load_measure=ice_load_measure,
+    odt_load_measure=ocean_dynamic_load_measure,
+)
+
+# %%
+slc = sea_level_change_measure(
+    fingerprint_operator=fingerprint_operator,
+    fingerprint=fp,
+    load_measure=direct_load_measure,
+)
+
+# %%
+
+ssh_measure, ssh_odt_measure, _ = sea_surface_height_measure(
+    fingerprint=fp,
+    fingerprint_operator=fingerprint_operator,
+    load_measure=direct_load_measure,
+    odt_measure=ocean_dynamic_measure,
+)
+
+# %%
+
+gmsl_true = get_gmsl_measure(
+    measure=slc,
+    fingerprint=fp,
+)
+gmsl_ssh_estimate = get_gmsl_measure(
+    measure=ssh_measure,
+    fingerprint=fp,
+)
+gmsl_ssh_odt_estimate = get_gmsl_measure(
+    measure=ssh_odt_measure,
+    fingerprint=fp,
+)
+
+# %% --- ALL PLOTTING BELOW ---
+
 if projection_plots:
     fig1, ax1, im1 = sl.plot(
         ocean_dynamic_load_measure.sample() * fp.load_scale,
@@ -78,17 +128,6 @@ if projection_plots:
         label="ODT Height",
         orientation="horizontal",
     )
-
-# %%
-ice_thickness_measure, ice_load_measure = (
-    ice_thickness_change_measures(
-        fingerprint=fp,
-        fingerprint_operator=fingerprint_operator,
-        length_scale=ice_length_scale,
-        ice_gmsl_target_std=ice_gmsl_target_std,
-        net_thickness_change=net_ice_thickness_change,
-    )
-)
 
 if projection_plots:
     fig2, ax2, im2 = sl.plot(
@@ -116,12 +155,6 @@ if projection_plots:
         orientation="horizontal",
     )
 
-# %%
-direct_load_measure = load_measure(
-    ice_thickness_load_measure=ice_load_measure,
-    odt_load_measure=ocean_dynamic_load_measure,
-)
-
 if projection_plots:
     fig3, ax3, im3 = sl.plot(
         direct_load_measure.sample() * fp.load_scale,
@@ -135,13 +168,6 @@ if projection_plots:
         orientation="horizontal",
     )
 
-# %%
-slc = sea_level_change_measure(
-    fingerprint_operator=fingerprint_operator,
-    fingerprint=fp,
-    load_measure=direct_load_measure,
-)
-
 if projection_plots:
     fig4, ax4, im4 = sl.plot(
         slc.sample() * fp.length_scale,
@@ -154,18 +180,7 @@ if projection_plots:
         orientation="horizontal",
     )
 
-# %%
-
-ssh_measure, ssh_odt_measure, _ = sea_surface_height_measure(
-    fingerprint=fp,
-    fingerprint_operator=fingerprint_operator,
-    load_measure=direct_load_measure,
-    odt_measure=ocean_dynamic_measure,
-)
-
-# %% Plot SSH
-
-# if projection_plots:
+# Plot SSH
 fig5, ax5, im5 = sl.plot(
     ssh_measure.sample() * fp.length_scale,
     symmetric=False,
@@ -189,19 +204,6 @@ fig6.colorbar(
 )
 
 # %%
-
-gmsl_true = get_gmsl_measure(
-    measure=slc,
-    fingerprint=fp,
-)
-gmsl_ssh_estimate = get_gmsl_measure(
-    measure=ssh_measure,
-    fingerprint=fp,
-)
-gmsl_ssh_odt_estimate = get_gmsl_measure(
-    measure=ssh_odt_measure,
-    fingerprint=fp,
-)
 
 plot_fig, plot_ax = plot_measure(
     measures=[
