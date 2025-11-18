15ac0ad5ed6d8139d151033f9b9ed2a6a7ecaf6c
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-18 17:42:46 +0000

    added more error plots

--- work/10-gmsl_error_quantification/error_plots.py
+++ work/10-gmsl_error_quantification/error_plots.py
@@ -1,7 +1,10 @@
 # %%
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
+from scipy.stats import norm
 
 mpl.rcParams["figure.dpi"] = 600
 
@@ -19,12 +22,9 @@ data = pd.read_csv(
 sns.pairplot(
     data,
     x_vars=[
-        "ice_length_scale",
         "ice_gmsl_target_std",
         "net_ice_thickness_change",
-        "odt_length_scale",
         "odt_standard_deviation",
-        "altimetry_error_length_scale",
         "altimetry_error_amplitude",
         "altimetry_range",
     ],
@@ -35,4 +35,39 @@ sns.pairplot(
 )
 
 # %%
-# Sensitivity analysis using SALib
+# for each of the input parameters, average the error_mean and error_std over the other parameters and plot each of the distributions using scipy norm
+
+input_parameters = [
+    "ice_gmsl_target_std",
+    "net_ice_thickness_change",
+    "odt_standard_deviation",
+    "altimetry_error_amplitude",
+    "altimetry_range",
+]
+
+for param in input_parameters:
+    grouped = data.groupby(param).agg(
+        {"error_mean": "mean", "error_std": "mean"},
+    )
+    # find the max and min y values of the distributions via 4* standard deviation
+    xmax = (
+        grouped["error_mean"].max() + 4 * grouped["error_std"].max()
+    )
+    xmin = (
+        grouped["error_mean"].min() - 4 * grouped["error_std"].max()
+    )
+    x = np.linspace(xmin, xmax, 1000)
+    plt.figure(figsize=(8, 6))
+    for i, row in grouped.iterrows():
+        mean = row["error_mean"]
+        std = row["error_std"]
+        plt.plot(
+            x,
+            norm.pdf(x, mean, std),
+            label=f"{param}={i:.4f}",
+        )
+    plt.title(f"GMSL Error Distribution varying {param}")
+    plt.xlabel("GMSL Error (m)")
+    plt.ylabel("Probability Density")
+    plt.legend()
+    plt.show()
