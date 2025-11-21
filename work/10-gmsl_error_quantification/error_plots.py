c93ceaa48c84a6cd553c0bbd1e4281c01e654236
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-21 09:45:18 +0000

    added plots with more dat

--- work/10-gmsl_error_quantification/error_plots.py
+++ work/10-gmsl_error_quantification/error_plots.py
@@ -1,16 +1,16 @@
 # %%
+import colorcet as cc
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.stats import norm
-import colorcet as cc
 from scipy.interpolate import griddata
+from scipy.stats import norm
 
 data = pd.read_csv(
-    "gmsl_error_with_measurement_noise_results_lmax64.csv",
+    "gmsl_error_with_measurement_noise_results_lmax128.csv",
 )
 
 # %%
@@ -51,17 +51,27 @@ for param in input_parameters:
         {"error_mean": "mean", "error_std": "mean"},
     )
     # find the max and min y values of the distributions via 4* standard deviation
-    xmax = grouped["error_mean"].max() + 4 * grouped["error_std"].max()
-    xmin = grouped["error_mean"].min() - 4 * grouped["error_std"].max()
+    xmax = (
+        grouped["error_mean"].max() + 4 * grouped["error_std"].max()
+    )
+    xmin = (
+        grouped["error_mean"].min() - 4 * grouped["error_std"].max()
+    )
     x = np.linspace(xmin, xmax, 1000)
     plt.figure(figsize=(8, 6))
-    for i, row in grouped.iterrows():
+    for k, (i, row) in enumerate(grouped.iterrows()):
         mean = row["error_mean"]
         std = row["error_std"]
+
+        # Use the sequential index 'k' for color mapping
+        color_value = k / len(grouped)
+
         plt.plot(
             x,
             norm.pdf(x, mean, std),
             label=f"{param}={i:.4f}",
+            # Use the sequential index for the color map
+            color=cc.cm.fire(color_value),
         )
     plt.title(f"GMSL Error Distribution varying {param}")
     plt.xlabel("GMSL Error (m)")
@@ -81,13 +91,19 @@ scatter = plt.scatter(
     data["altimetry_range"],
     # s=data["error_mean"] * 1000,`
     c=data["error_mean"],
-    cmap=cc.cm.CET_D13,
-    alpha=0.7,
+    cmap=cc.cm.CET_D1A,
+    norm=mpl.colors.TwoSlopeNorm(
+        vcenter=0,
+        vmin=-data["error_mean"].max(),
+        vmax=data["error_mean"].max(),
+    ),
 )
-plt.colorbar(scatter, label="Mean GMSL Error (m)")
-plt.xlabel("Net Ice Thickness Change (m/year)")
-plt.ylabel("Altimetry Range (m)")
-plt.title("Net Ice Thickness Change vs Altimetry Range with GMSL Error")
+plt.colorbar(scatter, label="GMSL Error Mean (m)")
+plt.xlabel("Net Ice Thickness Change (m)")
+plt.ylabel("Altimetry Range (deg)")
+plt.title(
+    "Net Ice Thickness Change vs Altimetry Range with GMSL Error Mean",
+)
 
 # do same plot but with but with continous colourisation across the entire data range using interpolation using pcolormesh
 plt.figure(figsize=(8, 6))
@@ -117,70 +133,31 @@ plt.pcolormesh(
     yi,
     zi,
     shading="auto",
-    cmap=cc.cm.CET_D13,
+    cmap=cc.cm.CET_D1A,
+    # set 0 as center of the colormap
+    norm=mpl.colors.TwoSlopeNorm(
+        vcenter=0,
+        vmin=-data["error_mean"].max(),
+        vmax=data["error_mean"].max(),
+    ),
 )
-plt.colorbar(label="Mean GMSL Error (m)")
-plt.xlabel("Net Ice Thickness Change (m/year)")
-plt.ylabel("Altimetry Range (m)")
-plt.title("Interpolated GMSL Error over Net Ice Thickness Change and Altimetry Range")
-
-# do same plot but with but with continous colourisation across the entire data range using interpolation using pcolormesh
-plt.figure(figsize=(8, 6))
-# create a grid of net_ice_thickness_change and altimetry_range
-xi = np.linspace(
-    data["net_ice_thickness_change"].min(),
-    data["net_ice_thickness_change"].max(),
-    100,
-)
-yi = np.linspace(
-    data["altimetry_range"].min(),
-    data["altimetry_range"].max(),
-    100,
-)
-xi, yi = np.meshgrid(xi, yi)
-# interpolate the error_mean onto the grid
-
-zi = griddata(
-    (data["net_ice_thickness_change"], data["altimetry_range"]),
-    np.abs(data["error_mean"]),
-    (xi, yi),
-    method="cubic",
-)
-# plot the interpolated data using pcolormesh
-plt.pcolormesh(
-    xi,
-    yi,
-    zi,
-    shading="auto",
-    cmap=cc.cm.CET_L17,
-)
-plt.colorbar(label="Absolute Mean GMSL Error (m)")
+plt.colorbar(label="GMSL Error Mean (m)")
 plt.xlabel("Net Ice Thickness Change (m)")
-plt.ylabel("Altimetry Range (+/- deg)")
-plt.title("GMSL Error over Net Ice Thickness Change and Altimetry Range")
-plt.savefig("gmsl_error_over_ice_change_and_altimetry_range.pdf", dpi=600)
-
-# %%
-# for 66 deg altimetry range, plot net_ice_thickness_change vs error_mean with error bars of error_std
-
-subset = data[data["altimetry_range"] == 66]
-plt.figure(figsize=(8, 6))
-plt.errorbar(
-    subset["net_ice_thickness_change"],
-    subset["error_mean"],
-    yerr=subset["error_std"],
-    fmt="o",
-    ecolor="r",
-    capsize=5,
+plt.ylabel("Altimetry Range (deg)")
+plt.title(
+    "Interpolated GMSL Error over Net Ice Thickness Change and Altimetry Range",
 )
-plt.xlabel("Net Ice Thickness Change (m)")
-plt.ylabel("Mean GMSL Error (m)")
-plt.title("GMSL Error vs Net Ice Thickness Change at 66 deg Altimetry Range")
-
+plt.savefig(
+    "gmsl_error_over_ice_change_and_altimetry_range.pdf",
+    dpi=600,
+)
+plt.savefig(
+    "gmsl_error_over_ice_change_and_altimetry_range.png",
+    dpi=600,
+)
 
 # %%
 # generate a 3D plot of net_ice_thickness_change vs altimetry_range vs error_mean with height being error_mean
-%matplotlib qt
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection="3d")
 ax.scatter(
@@ -188,20 +165,29 @@ ax.scatter(
     data["altimetry_range"],
     data["error_mean"],
     c=data["error_mean"],
-    cmap="viridis",
+    cmap=cc.cm.bmy,
     alpha=0.7,
 )
-ax.set_xlabel("Net Ice Thickness Change (m/year)")
-ax.set_ylabel("Altimetry Range (m)")
-ax.set_zlabel("Mean GMSL Error (m)")
-ax.set_title("3D Scatter Plot of GMSL Error")
-plt.show()
+ax.set_xlabel("Net Ice Thickness Change (m)")
+ax.set_ylabel("Altimetry Range (deg)")
+ax.set_zlabel("GMSL Error Mean (m)")
+ax.set_title("3D Scatter Plot of GMSL Error Mean")
 
+fig.colorbar(
+    ax.scatter(
+        data["net_ice_thickness_change"],
+        data["altimetry_range"],
+        data["error_mean"],
+        c=data["error_mean"],
+        cmap=cc.cm.bmy,
+        alpha=0.7,
+    ),
+    ax=ax,
+    label="GMSL Error Mean (m)",
+)
 
 
 # %%
-
-%matplotlib inline
 
 # plot the cdfs for varying net_ice_thickness_change average over other parameters
 
@@ -224,3 +210,8 @@ plt.title("GMSL Error CDFs varying net_ice_thickness_change")
 plt.xlabel("GMSL Error (m)")
 plt.ylabel("Cumulative Probability")
 plt.legend()
+
+
+# %%
+
+# linear model for error_mean as a function of the input parameters using scipy
