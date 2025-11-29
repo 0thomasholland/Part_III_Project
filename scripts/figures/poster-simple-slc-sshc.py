f82ced30ea62ab192b267b5ab9df0a0e8db465e3
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-11-29 14:32:03 +0000

    added some more figures

--- scripts/figures/poster-simple-slc-sshc.py
+++ scripts/figures/poster-simple-slc-sshc.py
@@ -1,5 +1,6 @@
 # %%
 import matplotlib as mpl
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 from pyshtools import SHGrid
@@ -47,17 +48,13 @@ sea_surface_height_change = fp.sea_surface_height_change(
 
 sea_level_change *= fp.ocean_function
 sea_surface_height_change *= fp.ocean_function
-
 error = sea_surface_height_change - sea_level_change
 
-gmsl = (
-    fp.integrate(sea_level_change * fp.ocean_function) / fp.ocean_area
-)
+gmsl = fp.integrate(sea_level_change) / fp.ocean_area
 gmsl_estimate = (
-    fp.integrate(sea_surface_height_change * fp.ocean_function)
-    / fp.ocean_area
+    fp.integrate(sea_surface_height_change) / fp.ocean_area
 )
-error_calc = fp.integrate(error * fp.ocean_function) / fp.ocean_area
+error_calc = fp.integrate(error) / fp.ocean_area
 alt_error = gmsl_estimate - gmsl
 
 print(f"GMSL: {gmsl:.6f} m")
@@ -65,11 +62,10 @@ print(f"GMSL Estimate: {gmsl_estimate:.6f} m")
 print(f"Error: {error_calc:.6f} m")
 print(f"Alternative Error: {alt_error:.6f} m")
 
-error *= fp.ocean_function
 
 # %%
-# for SLC and SSHC find the min and max and set a universal colorbar range
 
+# for SLC and SSHC find the min and max and set a universal colorbar range
 data_min = min(
     sea_level_change.min(),
     sea_surface_height_change.min(),
@@ -78,88 +74,118 @@ data_max = max(
     sea_level_change.max(),
     sea_surface_height_change.max(),
 )
+data_abs = 2.5
 
+print(f"SLC min: {(sea_level_change).min():.2e} m")
+print(f"SLC max: {sea_level_change.max():.2e} m")
+print(f"SSHC min: {sea_surface_height_change.min():.2e} m")
+print(f"SSHC max: {sea_surface_height_change.max():.2e} m")
+print(f"Universal colorbar range: ±{data_abs:.2e} m")
+
+# %%
+
+norm = mcolors.TwoSlopeNorm(vmin=-2.5, vcenter=0, vmax=0.3)
 
 # %%
 # Plotting
 
-plots = [
-    {
-        "data": ice_change,
-        "title": "Ice Thickness Change",
-        "mask": fp.ice_projection(),
-        "cbar_label": "m",
-        "symmetric_cbar": True,
-    },
-    {
-        "data": direct_load,
-        "title": "Direct Load from Ice Thickness Change",
-        "mask": fp.land_projection(),
-        "cbar_label": "kg/m$^2$",
-        "symmetric_cbar": True,
-    },
-    {
-        "data": sea_level_change * 1000,
-        "title": "Sea Level Change (SLC) Fingerprint",
-        "cbar_label": "mm",
-        "mask": fp.ocean_projection(),
-        "vmax": data_max * 1000,
-        "vmin": data_min * 1000,
-    },
-    {
-        "data": sea_surface_height_change * 1000,
-        "title": "Sea Surface Height Change (SSHC) Fingerprint",
-        "mask": fp.ocean_projection(),
-        "cbar_label": "mm",
-        "vmax": data_max * 1000,
-        "vmin": data_min * 1000,
-    },
-    {
-        "data": error * 1000,
-        "title": "Error (SSHC - SLC)",
-        "mask": fp.ocean_projection(),
-        "cbar_label": "mm",
-        "log_norm": True,
-    },
-]
+
+fig_ice, ax_ice, im_ice = plot(
+    ice_change * fp.ice_projection(),
+    symmetric=True,
+)
+cbar_ice = fig_ice.colorbar(
+    im_ice,
+    ax=ax_ice,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_ice.set_label("Ice Thickness Change (m)")
+
+fig_load, ax_load, im_load = plot(
+    direct_load * 1e-6 * fp.ice_projection(),
+    symmetric=True,
+)
+cbar_load = fig_load.colorbar(
+    im_load,
+    ax=ax_load,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_load.set_label("Load Change (MPa)")
+
+fig_displacement, ax_displacement, im_displacement = plot(
+    displacement,
+    symmetric=True,
+)
+cbar_displacement = fig_displacement.colorbar(
+    im_displacement,
+    ax=ax_displacement,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_displacement.set_label("Vertical Displacement (m)")
 
 
-for i, plot_info in enumerate(plots):
-    if plot_info.get("vmax", None) is None:
-        fig, ax, im = plot(
-            plot_info["data"] * plot_info.get("mask", 1),
-            symmetric=plot_info.get("symmetric_cbar", False),
-        )
-    else:
-        fig, ax, im = plot(
-            plot_info["data"] * plot_info.get("mask", 1),
-            symmetric=plot_info.get("symmetric_cbar", False),
-            vmax=plot_info["vmax"],
-            vmin=plot_info["vmin"],
-        )
-    if plot_info.get("log_norm", False):
-        im.set_norm(
-            mpl.colors.SymLogNorm(
-                linthresh=0.01 * np.max(np.abs(plot_info["data"])),
-                linscale=0.1,
-                vmin=np.min(plot_info["data"]),
-                vmax=np.max(plot_info["data"]),
-                base=10,
-            ),
-        )
-    fig.colorbar(
-        im,
-        ax=ax,
-        label=plot_info["cbar_label"],
-        orientation="horizontal",
-        pad=0.1,
-    )
-    plt.title(plot_info["title"])
-    plt.tight_layout()
-    plt.savefig(
-        f"../../outputs/poster/AutomatedFigures/poster_simple_slc_sshc_{i + 1}.png",
-        dpi=600,
-        bbox_inches="tight",
-    )
-    plt.show()
-    plt.close()
+fig_slc, ax_slc, im_slc = plot(
+    sea_level_change * fp.ocean_projection(),
+    norm=norm,
+)
+cbar_slc = fig_slc.colorbar(
+    im_slc,
+    ax=ax_slc,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_slc.set_ticks([-2.5, -1.25, 0, 0.3])
+cbar_slc.set_label("Sea Level Change (m)")
+
+fig_sshc, ax_sshc, im_sshc = plot(
+    sea_surface_height_change * fp.ocean_projection(),
+    norm=norm,
+)
+cbar_sshc = fig_sshc.colorbar(
+    im_sshc,
+    ax=ax_sshc,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_sshc.set_ticks([-2.5, -1.25, 0, 0.3])
+cbar_sshc.set_label("Sea Surface Height Change (m)")
+
+# %%
+fig_error, ax_error, im_error = plot(
+    error * fp.ocean_projection(),
+    symmetric=True,
+    vmax=1.5,
+    vmin=-1.5,
+)
+cbar_error = fig_error.colorbar(
+    im_error,
+    ax=ax_error,
+    orientation="horizontal",
+    pad=0.05,
+    shrink=0.7,
+)
+cbar_error.set_label("Error (m) [SSHC - SLC]")
+
+# %%
+# save all the figures at 600 dpi in ../../outputs/posters/AutomatedFigures/
+output_path = "../../outputs/poster/AutomatedFigures/Simple/"
+fig_ice.savefig(output_path + "1-ice_change.png", dpi=600)
+fig_load.savefig(output_path + "2-direct_load.png", dpi=600)
+fig_displacement.savefig(
+    output_path + "3-vertical_displacement.png",
+    dpi=600,
+)
+fig_slc.savefig(output_path + "4-sea_level_change.png", dpi=600)
+fig_sshc.savefig(
+    output_path + "5-sea_surface_height_change.png",
+    dpi=600,
+)
+fig_error.savefig(output_path + "6-error_sshc_slc.png", dpi=600)
