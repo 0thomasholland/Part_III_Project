3c1042a40f2dd53d97b3387465305a3d9ef3c3a7
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-12-01 10:48:45 +0000

    added new combined plots for distributions

---
+++ scripts/figures/poster-error-figs_comb.py
@@ -0,0 +1,473 @@
+# %%
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib as mpl
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import numpy as np
+from cartopy.mpl.ticker import LatitudeFormatter, LongitudeFormatter
+from pyslfp import (
+    FingerPrint,
+    averaging_operator,
+    sea_level_change_to_load_operator,
+    sea_surface_height_operator,
+    spatial_mutliplication_operator,
+)
+from scipy.stats import norm
+
+from Part_III_Project import (
+    ice_thickness_change_measures,
+    ocean_dynamic_topography_measures,
+)
+
+mpl.rcParams["font.size"] = 16
+mpl.rcParams["figure.dpi"] = 600
+output_dir = "../../outputs/poster/AutomatedFigures/Distributions_Flow/combined"
+
+# %%
+# Setup
+lmax = 256
+fp = FingerPrint(lmax=lmax)
+fp.set_state_from_ice_ng()
+fingerprint_operator = fp.as_sobolev_linear_operator(
+    2,
+    0.1 * fp.mean_sea_floor_radius,
+)
+load_space = fingerprint_operator.domain
+response_space = fingerprint_operator.codomain
+sea_surface_height_op = sea_surface_height_operator(
+    fp,
+    response_space,
+)
+measurement_space = sea_surface_height_op.codomain
+
+# %%
+# Parameters
+ice_length_scale = 0.5 * fp.mean_sea_floor_radius
+ice_gmsl_target_std = 0.005
+net_ice_thickness_change = -5.0
+
+odt_length_scale = 0.1 * fp.mean_sea_floor_radius
+odt_standard_deviation_factor = 0.001 / fp.length_scale
+
+altimetry_error_length_scale = 0.01 * fp.mean_sea_floor_radius
+altimetry_error_standard_deviation = 0.0005 / fp.length_scale
+
+altimetry_range = 66
+
+# %%
+# Generate data
+ice_thickness_change_measure, ice_direct_load_change_measure = (
+    ice_thickness_change_measures(
+        fp,
+        fingerprint_operator,
+        ice_length_scale,
+        ice_gmsl_target_std,
+        net_ice_thickness_change,
+    )
+)
+
+ice_thickness_change_measure_sample = (
+    ice_thickness_change_measure.sample()
+)
+ice_load_measure_sample = fp.direct_load_from_ice_thickness_change(
+    ice_thickness_change_measure_sample,
+)
+ice_slc_sample_response = fp(direct_load=ice_load_measure_sample)
+ice_ssh_sample = fp.sea_surface_height_change(
+    ice_slc_sample_response[0],
+    ice_slc_sample_response[1],
+    ice_slc_sample_response[3],
+)
+ice_slc_sample = ice_slc_sample_response[0]
+
+ice_slc_measure = ice_direct_load_change_measure.affine_mapping(
+    operator=response_space.subspace_projection(0)
+    @ fingerprint_operator,
+)
+
+ice_ssh_measure = ice_direct_load_change_measure.affine_mapping(
+    operator=sea_surface_height_op @ fingerprint_operator,
+)
+
+odt_measure, odt_load_measure = ocean_dynamic_topography_measures(
+    fp,
+    fingerprint_operator,
+    odt_length_scale,
+    odt_standard_deviation_factor,
+)
+
+odt_measure_sample = odt_measure.sample()
+odt_load_sample = fp.direct_load_from_sea_level_change(
+    odt_measure_sample,
+)
+odt_fingerprint_response = fp(direct_load=odt_load_sample)
+odt_fingerprint_sample = fp.sea_surface_height_change(
+    odt_fingerprint_response[0],
+    odt_fingerprint_response[1],
+    odt_fingerprint_response[3],
+)
+odt_error_sample = odt_fingerprint_sample + odt_measure_sample
+
+sea_level_to_load_op = sea_level_change_to_load_operator(
+    fp,
+    load_space,
+)
+odt_combined_op = (
+    sea_surface_height_op
+    @ fingerprint_operator
+    @ sea_level_to_load_op
+    + odt_measure.domain.identity_operator()
+)
+odt_error_measure = odt_measure.affine_mapping(
+    operator=odt_combined_op,
+)
+
+altimetry_error_measure = measurement_space.point_value_scaled_sobolev_kernel_gaussian_measure(
+    1.5,
+    altimetry_error_length_scale,
+    altimetry_error_standard_deviation,
+)
+altimetry_error_sample = altimetry_error_measure.sample()
+
+combined_error_measure = odt_error_measure + altimetry_error_measure
+combined_error_sample = odt_error_sample + altimetry_error_sample
+
+total_observerable_measure = ice_ssh_measure + combined_error_measure
+total_observable_sample = ice_ssh_sample + combined_error_sample
+
+altimetry_projection = fp.altimetry_projection(
+    latitude_min=-altimetry_range,
+    latitude_max=altimetry_range,
+    value=0,
+) * fp.ocean_projection(value=0)
+
+altimetry_projection_for_plotting = fp.altimetry_projection(
+    latitude_min=-altimetry_range,
+    latitude_max=altimetry_range,
+    value=np.nan,
+) * fp.ocean_projection(value=np.nan)
+
+altimetry_operator = spatial_mutliplication_operator(
+    altimetry_projection,
+    measurement_space,
+)
+
+total_observed_measure = total_observerable_measure.affine_mapping(
+    operator=altimetry_operator,
+)
+total_observed_sample = total_observable_sample * altimetry_projection
+
+# %%
+# Calculate Ice GMSL standard deviation for secondary axis
+ice_gmsl_spatial_average = averaging_operator(
+    ice_slc_measure.domain,
+    [
+        fp.ocean_projection(value=0)
+        / fp.integrate(fp.ocean_projection(value=0)),
+    ],
+)
+ice_gmsl_averaged_measure = ice_slc_measure.affine_mapping(
+    operator=ice_gmsl_spatial_average,
+)
+ice_gmsl_std = (
+    ice_gmsl_averaged_measure.covariance.matrix(dense=True)[0, 0]
+    ** 0.5
+)
+ice_gmsl_std_scaled = ice_gmsl_std * 1000.0 * fp.length_scale
+
+
+# %%
+def create_composite_figure(
+    shgrid_data,
+    map_title,
+    map_units,
+    dist_expectation,
+    dist_std,
+    ice_gmsl_std_scaled,
+    title_color="black",
+    bg_color=None,
+    dist_color="black",
+    projection=ccrs.Robinson(),
+    cmap="RdBu",
+    symmetric=True,
+    coasts=True,
+    show_secondary_axis=True,
+):
+    """Create a square composite figure with map and inset distribution."""
+    # Create square figure
+    fig_size = 10
+    fig = plt.figure(figsize=(fig_size, fig_size))
+
+    # Set background color
+    if bg_color is not None:
+        fig.set_facecolor(mcolors.to_rgba(bg_color, alpha=0.2))
+
+    # Create main map axes - nearly full figure
+    ax_map = fig.add_axes(
+        [0.02, 0.0, 0.96, 0.88],
+        projection=projection,
+    )
+
+    # Get lon/lat from SHGrid
+    lons = shgrid_data.lons()
+    lats = shgrid_data.lats()
+    data = shgrid_data.data
+
+    # Set up color scaling
+    plot_kwargs = {"cmap": cmap}
+    if symmetric:
+        data_max = 1.2 * np.nanmax(np.abs(data))
+        plot_kwargs["vmin"] = -data_max
+        plot_kwargs["vmax"] = data_max
+
+    # Plot the data
+    im = ax_map.pcolormesh(
+        lons,
+        lats,
+        data,
+        transform=ccrs.PlateCarree(),
+        **plot_kwargs,
+    )
+
+    # Add coastlines
+    if coasts:
+        ax_map.add_feature(cfeature.COASTLINE, linewidth=0.8)
+
+    # Set global extent
+    ax_map.set_global()
+
+    # Add title
+    ax_map.set_title(map_title, fontsize=24)
+
+    # Create inset axes for distribution - overlapping bottom left of map
+    # [left, bottom, width, height]
+    ax_inset = fig.add_axes([0.06, 0.1, 0.28, 0.22])
+    ax_inset.set_facecolor(mcolors.to_rgba("white", alpha=0.6))
+
+    # Add horizontal colorbar next to inset plot
+    # [left, bottom, width, height]
+    cbar_ax = fig.add_axes([0.45, 0.15, 0.45, 0.025])
+    cbar = fig.colorbar(im, cax=cbar_ax, orientation="horizontal")
+    cbar.set_label(map_units)
+
+    # Plot distribution
+    x_space = np.linspace(
+        dist_expectation - 4 * dist_std,
+        dist_expectation + 4 * dist_std,
+        100,
+    )
+    pdf = norm.pdf(x_space, loc=dist_expectation, scale=dist_std)
+
+    ax_inset.plot(x_space, pdf, color=dist_color, linewidth=2)
+    ax_inset.set_xlabel(map_units, fontsize=12)
+    ax_inset.set_ylabel("Probability Density", fontsize=10)
+
+    # Add secondary x-axis if requested and units are mm
+    if show_secondary_axis and map_units == "mm":
+        ax_inset_sec = ax_inset.secondary_xaxis(
+            -0.15,
+            functions=(
+                lambda x, e=dist_expectation, s=ice_gmsl_std_scaled: (
+                    x - e
+                )
+                / s,
+                lambda x, e=dist_expectation, s=ice_gmsl_std_scaled: x
+                * s
+                + e,
+            ),
+        )
+        ax_inset_sec.set_xlabel(
+            "Relative to Ice GMSL (σ)",
+            fontsize=10,
+        )
+        ax_inset_sec.tick_params(labelsize=10)
+
+    # Style the inset
+    ax_inset.tick_params(labelsize=10)
+
+    # Add border to inset
+    for spine in ax_inset.spines.values():
+        spine.set_edgecolor("gray")
+        spine.set_linewidth(1)
+
+    return fig, ax_map, ax_inset
+
+
+def get_measure_stats(measure, projection, scale_factor, fp):
+    """Calculate expectation and standard deviation for a measure."""
+    spatial_average = averaging_operator(
+        measure.domain,
+        [projection / fp.integrate(projection)],
+    )
+    averaged_measure = measure.affine_mapping(
+        operator=spatial_average,
+    )
+    expectation = (
+        averaged_measure.expectation[0]
+        * scale_factor
+        * fp.length_scale
+    )
+    std = (
+        averaged_measure.covariance.matrix(dense=True)[0, 0] ** 0.5
+        * scale_factor
+        * fp.length_scale
+    )
+    return expectation, std
+
+
+# %%
+# Define all plot configurations
+# Each entry: (
+#     title,
+#     map_sample,
+#     measure,
+#     projection_for_avg,
+#     units,
+#     scale_factor,
+#     title_color,
+#     bg_color,
+# )
+
+composite_plots = [
+    (
+        "Ice Thickness Change",
+        ice_thickness_change_measure_sample * fp.ice_projection(),
+        ice_thickness_change_measure,
+        fp.ice_projection(value=0),
+        "m",
+        1.0,
+        "black",
+        None,
+    ),
+    (
+        "Sea Level Change from Ice Load",
+        ice_slc_sample * fp.ocean_projection() * 1000,
+        ice_slc_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "tab:blue",
+        "tab:blue",
+    ),
+    (
+        "Sea Surface Height Change from Ice Load",
+        ice_ssh_sample * fp.ocean_projection() * 1000,
+        ice_ssh_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "black",
+        None,
+    ),
+    (
+        "Ocean Dynamic Topography Change",
+        odt_measure_sample * fp.ocean_projection() * 1000,
+        odt_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "black",
+        None,
+    ),
+    (
+        "ODT Error",
+        odt_error_sample * fp.ocean_projection() * 1000,
+        odt_error_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "black",
+        None,
+    ),
+    (
+        "Altimetry Error",
+        altimetry_error_sample * fp.ocean_projection() * 1000,
+        altimetry_error_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "black",
+        None,
+    ),
+    (
+        "Combined Ocean Error",
+        combined_error_sample * fp.ocean_projection() * 1000,
+        combined_error_measure,
+        fp.ocean_projection(value=0),
+        "mm",
+        1000.0,
+        "black",
+        None,
+    ),
+    (
+        "Total Observed Sea Surface Height Change",
+        total_observed_sample
+        * altimetry_projection_for_plotting
+        * 1000,
+        total_observed_measure,
+        altimetry_projection,
+        "mm",
+        1000.0,
+        "tab:orange",
+        "tab:orange",
+    ),
+]
+
+# %%
+# Generate all composite figures
+for plot_config in composite_plots:
+    (
+        title,
+        map_sample,
+        measure,
+        projection_for_avg,
+        units,
+        scale_factor,
+        title_color,
+        bg_color,
+    ) = plot_config
+
+    # Calculate distribution statistics
+    expectation, std = get_measure_stats(
+        measure,
+        projection_for_avg,
+        scale_factor,
+        fp,
+    )
+
+    print(
+        f"{title}: Expectation = {expectation:.4e} {units}, Std = {std:.4e} {units}",
+    )
+
+    # Determine distribution color (same as title color, or black if None)
+    dist_color = title_color if title_color != "black" else "black"
+
+    # Create composite figure
+    fig, ax_map, ax_inset = create_composite_figure(
+        shgrid_data=map_sample,
+        map_title=title,
+        map_units=units,
+        dist_expectation=expectation,
+        dist_std=std,
+        ice_gmsl_std_scaled=ice_gmsl_std_scaled,
+        title_color=title_color,
+        bg_color=bg_color,
+        dist_color=dist_color,
+    )
+
+    # Save figure
+    filename = title.lower().replace(" ", "_")
+    try:
+        plt.savefig(
+            f"{output_dir}/{filename}_composite.png",
+            dpi=600,
+            bbox_inches="tight",
+        )
+    except Exception as e:
+        print(f"  Could not save figure: {e}")
+
+    plt.close()
+
+print("\nAll composite figures generated!")
