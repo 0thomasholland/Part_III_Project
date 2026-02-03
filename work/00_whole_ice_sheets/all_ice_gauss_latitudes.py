Fix Gaussian latitude sweep computation and plotting output

work/00_whole_ice_sheets/all_ice_gauss_latitudes.py
@@ -23,7 +23,7 @@ alimetry_resolution = (
     100  # number of points from 0 to 90˚ that are sampled
 )
 
-latitudes = np.linspace(1, 90, alimetry_resolution)
+latitudes = np.linspace(10, 90, alimetry_resolution)
 gmsl_target_mean = np.array([0, 0.001, 0.01])
 gmsl_target_std = np.array([0.001, 0.005])
 