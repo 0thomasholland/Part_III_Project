bdbd90e791d5f99470d660ded3724e8162275f4f
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2026-02-06 22:27:50 +0000

    Remove duplicated Gaussian altimetry code and consolidate into pyslfp_extras

--- src/pyslfp_extras/operators.py
+++ src/pyslfp_extras/operators.py
@@ -2,11 +2,16 @@ import numpy as np
 from pygeoinf import HilbertSpace, LinearOperator
 from pyslfp import FingerPrint
 
+from pyslfp_extras.helpers import (
+    get_ocean_point_coordinates,
+)
+
 
 def ocean_point_evaluation_operator(
     finger_print: FingerPrint,
     measurement_space: HilbertSpace,
     point_degree_spacing: float = 5.0,
+    altimetry_latitude_range: float = 66.0,
 ) -> LinearOperator:
     """
     Constructs a linear operator that evaluates the ocean surface height at
@@ -15,38 +20,13 @@ def ocean_point_evaluation_operator(
     classified as ocean according to the `FingerPrint`'s ocean and altimetry
     projections.
     """
-    mask = (
-        finger_print.ocean_projection(value=0)
-        * finger_print.altimetry_projection(value=0)
-    ).to_array()
-    nlat, nlon = mask.shape
-
-    mask_lats = np.linspace(90, -90, nlat)
-    mask_lons = (
-        np.linspace(0, 360, nlon, endpoint=True)
-        if nlon > nlat
-        else np.linspace(0, 360, nlon, endpoint=False)
+    ocean_coords = get_ocean_point_coordinates(
+        finger_print,
+        point_degree_spacing=point_degree_spacing,
+        altimetry_latitude_range=altimetry_latitude_range,
     )
 
-    target_lats = np.arange(
-        90,
-        -90 - point_degree_spacing,
-        -point_degree_spacing,
-    )
-    target_lons = np.arange(0, 360, point_degree_spacing)
-
-    ocean_coords = []
-    for lat in target_lats:
-        for lon in target_lons:
-            mask_lat_idx = np.argmin(
-                np.abs(mask_lats - lat)
-            )
-            mask_lon_idx = np.argmin(
-                np.abs(mask_lons - lon)
-            )
-
-            if mask[mask_lat_idx, mask_lon_idx] == 1:
-                ocean_coords.append([lat, lon])
+    ocean_coords = list(zip(*ocean_coords))
 
     _op = measurement_space.point_evaluation_operator(
         ocean_coords
