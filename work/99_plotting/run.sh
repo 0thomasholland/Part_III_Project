df0654f5e16b9f2f256eb98c667730a9c4ba91c4
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2026-03-13 21:10:11 +0000

    Extend run.sh and regenerate sensitivity KDE figures

---
+++ work/99_plotting/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#uv run 01_sea_surface_height_plots.py
+echo Starting 2
+uv run 02_major_sources_plots.py
+echo "Starting 5"
+uv run 05_altimetry_sampling_plots.py
+echo Starting 6
+uv run 06_simple_inversion_plots.py
+echo Starting 7
+uv run 07_inversion_sensitivity_plots.py
+echo Starting 9
+uv run 09_truth_prior_cross_test_plots.py
+
+echo Moving files
+../../copy_report_pdfs.sh
