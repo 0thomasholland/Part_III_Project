a95c6a0b8b9fb771de0961c6e0be7205f31fe6ea
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2026-03-17 09:49:49 +0000

    Fix import path in factored joint inversion script

--- work/08_joint_inversion/joint_precon_factored.py
+++ work/08_joint_inversion/joint_precon_factored.py
@@ -31,7 +31,6 @@ from project.operators import (
 )
 from pygeoinf_extras import standard_dev
 from pygeoinf_extras.plots import plot_bivariate_corner
-from pyslfp_extras import colors
 from pyslfp_extras.altimetry import GridPoints
 from pyslfp_extras.ice_thickness import IceSheetChange
 from pyslfp_extras.ocean_dynamics import OceanDynamics
