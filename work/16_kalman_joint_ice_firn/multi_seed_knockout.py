4c9c228793e3cf3ea2ef6492246871daebcc2942
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2026-06-27 16:35:14 +0100

    update

--- work/16_kalman_joint_ice_firn/multi_seed_knockout.py
+++ work/16_kalman_joint_ice_firn/multi_seed_knockout.py
@@ -4,7 +4,6 @@ from dataclasses import replace
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 from common import ExperimentConfig, ensure_output_dir, run_variant
